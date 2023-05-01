4272289615b05704432c640ebd3db2f41632032f
Geoff Fite <GFite@users.noreply.github.com>
2023-05-01 01:06:27 -0700

    commands

--- src/auto_gpt_finx/finx-gpt.py
+++ src/auto_gpt_finx/finx-gpt.py
@@ -1,12 +1,17 @@
 #! python
-from typing import Any, Dict, List, Optional, Tuple, TypeVar, TypedDict
-import requests
+from typing import Any, Dict, Optional
+import os
 from . import AutoGPTFinX
 import finx
+from finx.client import FinXClient
 
 plugin = AutoGPTFinX()
 
 def analyze_investment_security(self, security_id: str, as_of_date: str) -> Optional[Dict[str, Any]]:
     """Analyze security using FinX API"""
-    results = finx.calculate_security_analytics(security_id, as_of_date)
+    finx_api_key = os.getenv("FINX_API_KEY")
+    if not finx_api_key:
+        raise Exception("FINX_API_KEY not set")
+    finx_client = FinXClient("socket", ssl=True, api_key=finx_api_key)
+    results = finx_client.calculate_security_analytics(security_id, as_of_date)
     return results
