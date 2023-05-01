8503919b2d404f40e99d221b78a68270c5abefcf
Geoff Fite <GFite@users.noreply.github.com>
2023-04-30 22:35:40 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "FinX Plugin For Auto-GPT Installation Script"
+echo "Please follow the installation prompts:"
+echo newline
+echo "Enter the full path of your Auto-GPT workspace (ex: /home/ubuntu/Auto-GPT/auto_gpt_workspace)"
+read workspace
+if [[ "$(workspace: -1)" == "/" ]]; then
+  workspace="$(workspace:0:-1)"
+fi
+cp requirements.txt $workspace/finx-requirements.txt
+cd $workspace
+echo "FinX Plugin for Auto-GPT will now install dependencies."
+pip install -r finx-requirements.txt
+echo "FinX Plugin for Auto-GPT dependency installation complete."
+echo "Now you will need to configure your Auto-GPT Environment (default is Auto-GPT/.env)"
+echo "Please enter the following values into your .env file"
+echo "*****************************************************"
+echo "FINX_API_KEY=slug_my_finx_api_key"
+echo "FINX_API_EMAIL=slug_registered_email"
+echo "FINX_DATA_PATH=slug_my_workspace_path"
+echo "*****************************************************"
+echo "One you have entered the values, replace the slug values with you actual FinX API Key, email and data path."
